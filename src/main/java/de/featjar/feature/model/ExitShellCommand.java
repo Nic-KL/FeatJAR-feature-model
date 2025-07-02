75b5a99759dc14a4b23cb0994e8d2db482237f45
Niclas <niclas.k@mailbox.org>
2025-07-02 11:39:26 +0200

    refactor: removed duplicated code and simplified some method calls

--- src/main/java/de/featjar/feature/model/ExitShellCommand.java
+++ src/main/java/de/featjar/feature/model/ExitShellCommand.java
@@ -10,18 +10,14 @@ public class ExitShellCommand implements IShellCommand {
 	
 	@Override
 	public void execute(ShellSession session) {		
-		System.out.println(" _____             _       _    _     ____   ____   _            _  _ ");
-		System.out.println("|  ___|___   __ _ | |_    | |  / \\   |  _ \\ / ___| | |__    ___ | || |");
-		System.out.println("| |_  / _ \\ / _` || __|_  | | / _ \\  | |_) |\\___ \\ | '_ \\  / _ \\| || |");
-		System.out.println("|  _||  __/| (_| || |_| |_| |/ ___ \\ |  _ <  ___) || | | ||  __/| || |");
-		System.out.println("|_|   \\___| \\__,_| \\__|\\___//_/   \\_\\|_| \\_\\|____/ |_| |_| \\___||_||_|");
-		System.out.println("");
+		Shell.printArt();
 		System.exit(0);
 	}	
     @Override
     public Optional<String> getShortName() {
         return Optional.of("exit");
     }
+    @Override
     public Optional<String> getDescription(){
     	return Optional.of("leave shell");
     }
