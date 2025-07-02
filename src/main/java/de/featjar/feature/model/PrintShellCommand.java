2e6d59ba8c7488b538ed9c6afbbcdee5d0e53735
Niclas <niclas.k@mailbox.org>
2025-07-02 10:32:14 +0200

    feat : new shell commands

--- src/main/java/de/featjar/feature/model/PrintShellCommand.java
+++ src/main/java/de/featjar/feature/model/PrintShellCommand.java
@@ -1,13 +1,17 @@
 package de.featjar.feature.model;
 
+import java.util.Optional;
+
 import de.featjar.base.FeatJAR;
 
-public class PrintShellCommand extends AbstractShellCommand {
+public class PrintShellCommand implements IShellCommand {
 
 	@Override
 	public void execute(ShellSession session) {
-		System.out.println(FeatJAR.extensionPoint(ShellCommands.class).getExtensions().stream());
-		
+		FeatJAR.extensionPoint(ShellCommands.class).getExtensions().stream().map(c -> c.getIdentifier()).forEach(System.out::println);		
 	}
-
+    @Override
+    public Optional<String> getShortName() {
+        return Optional.of("print");
+    }
 }
