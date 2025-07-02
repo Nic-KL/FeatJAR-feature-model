da6ca442a85e8fae23676f165e68627fc211c783
Niclas <niclas.k@mailbox.org>
2025-07-02 10:32:14 +0200

    feat : new shell commands

--- src/main/java/de/featjar/feature/model/StoreShellCommand.java
+++ src/main/java/de/featjar/feature/model/StoreShellCommand.java
@@ -1,28 +1,45 @@
 package de.featjar.feature.model;
 
+import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-public class StoreShellCommand extends AbstractShellCommand {	
-	private final Map<String, StoreShellCommand> subCommands = Map.of(
-			"1", (ctx, arg) -> );
+import de.featjar.base.FeatJAR;
+import de.featjar.base.data.Result;
+import de.featjar.base.io.IO;
+import de.featjar.base.io.format.IFormat;
+import de.featjar.base.io.text.StringTextFormat;
+import de.featjar.feature.model.io.FeatureModelFormats;
+import de.featjar.feature.model.io.xml.XMLFeatureModelFormat;
+import de.featjar.formula.io.csv.BooleanAssignmentGroupsCSVFormat;
+import de.featjar.formula.io.dimacs.FormulaDimacsFormat;
 
+// TODO nicht alle Vriablen sondern nur X gleiches für load, Format dass alles als String speichert, autocomplete und dann erst weiter \r dann geht er einen zurück in der konsole, globales abort
+
+public class StoreShellCommand implements IShellCommand {	
+	
 	public StoreShellCommand() {
 		super();
 	}
 
-	@Override
-	public void execute() {
-		// TODO Typecheck
-		
-
+	public static <T> void save(T res, Path path, IFormat<T> format) {		
+		try {
+			IO.save(res, path, format);
+		} catch (IOException e) {
+			FeatJAR.log().error(e);
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void execute(Path p) {
-		System.out.println("Usage: load <type> <path>");
+	public void execute(ShellSession session) {		
+//		StringTextFormat st = new StringTextFormat();
+
+		System.out.println(session.listOfSampleLists.get(0));
+		session.listOfSampleLists.forEach(t -> save(t, Paths.get("/home/hans/Dokumente/Uni/Arbeit/Sebastian/Neu/FeatJAR/feature-model/abc.xml"), new BooleanAssignmentGroupsCSVFormat()));
 	}
 	
     @Override
